070cf8da881a5a1163087b805f60341098f7603e
riowang(王腾) <riowang@futunn.com>
2025-04-03 17:14:51 +0800

    修订nodejs的版本

--- client/src/components/klineBox.vue
+++ client/src/components/klineBox.vue
@@ -81,209 +81,207 @@ const computeOption = (rawData:any[]) => {
     return result;
   }
   var data = splitData(rawData);
-  myChart.setOption(
-    (option = {
-      animation: false,
-      legend: {
-        bottom: 10,
-        left: 'center',
-        data: ['Dow-Jones index', 'MA5', 'MA10', 'MA20', 'MA30']
-      },
-      tooltip: {
-        trigger: 'axis',
-        axisPointer: {
-          type: 'cross'
-        },
-        borderWidth: 1,
-        borderColor: '#ccc',
-        padding: 10,
-        textStyle: {
-          color: '#000'
-        },
-        position: function (pos: number[], _params: any, _el: any, _elRect: any, size: { viewSize: number[]; }) {
-          const obj = {
-            top: 10
-          };
-          // 不需要eslint的检查
-          // @ts-ignore
-          obj[['left', 'right'][+(pos[0] < size.viewSize[0] / 2)]] = 30;
-          return obj;
-        }
-        // extraCssText: 'width: 170px'
-      },
+  const chartOption = {
+    animation: false,
+    legend: {
+      bottom: 10,
+      left: 'center',
+      data: ['Dow-Jones index', 'MA5', 'MA10', 'MA20', 'MA30']
+    },
+    tooltip: {
+      trigger: 'axis',
       axisPointer: {
-        link: [
-          {
-            xAxisIndex: 'all'
-          }
-        ],
-        label: {
-          backgroundColor: '#777'
-        }
+        type: 'cross'
       },
-      toolbox: {
-        feature: {
-          dataZoom: {
-            yAxisIndex: false
-          },
-          brush: {
-            type: ['lineX', 'clear']
-          }
-        }
+      borderWidth: 1,
+      borderColor: '#ccc',
+      padding: 10,
+      textStyle: {
+        color: '#000'
       },
-      brush: {
-        xAxisIndex: 'all',
-        brushLink: 'all',
-        outOfBrush: {
-          colorAlpha: 0.1
-        }
-      },
-      visualMap: {
-        show: false,
-        seriesIndex: 5,
-        dimension: 2,
-        pieces: [
-          {
-            value: 1,
-            color: downColor
-          },
-          {
-            value: -1,
-            color: upColor
-          }
-        ]
-      },
-      grid: [
+      position: function (pos: number[], _params: any, _el: any, _elRect: any, size: { viewSize: number[]; }) {
+        const obj = {
+          top: 10
+        };
+        // 不需要eslint的检查
+        // @ts-ignore
+        obj[['left', 'right'][+(pos[0] < size.viewSize[0] / 2)]] = 30;
+        return obj;
+      }
+      // extraCssText: 'width: 170px'
+    },
+    axisPointer: {
+      link: [
         {
-          left: '10%',
-          right: '8%',
-          height: '50%'
-        },
-        {
-          left: '10%',
-          right: '8%',
-          top: '63%',
-          height: '16%'
+          xAxisIndex: 'all'
         }
       ],
-      xAxis: [
-        {
-          type: 'category',
-          data: data.categoryData,
-          boundaryGap: false,
-          axisLine: { onZero: false },
-          splitLine: { show: false },
-          min: 'dataMin',
-          max: 'dataMax',
-          axisPointer: {
-            z: 100
-          }
+      label: {
+        backgroundColor: '#777'
+      }
+    },
+    toolbox: {
+      feature: {
+        dataZoom: {
+          yAxisIndex: false
         },
-        {
-          type: 'category',
-          gridIndex: 1,
-          data: data.categoryData,
-          boundaryGap: false,
-          axisLine: { onZero: false },
-          axisTick: { show: false },
-          splitLine: { show: false },
-          axisLabel: { show: false },
-          min: 'dataMin',
-          max: 'dataMax'
+        brush: {
+          type: ['lineX', 'clear']
         }
-      ],
-      yAxis: [
+      }
+    },
+    brush: {
+      xAxisIndex: 'all',
+      brushLink: 'all',
+      outOfBrush: {
+        colorAlpha: 0.1
+      }
+    },
+    visualMap: {
+      show: false,
+      seriesIndex: 5,
+      dimension: 2,
+      pieces: [
         {
-          scale: true,
-          splitArea: {
-            show: true
-          }
+          value: 1,
+          color: downColor
         },
         {
-          scale: true,
-          gridIndex: 1,
-          splitNumber: 2,
-          axisLabel: { show: false },
-          axisLine: { show: false },
-          axisTick: { show: false },
-          splitLine: { show: false }
-        }
-      ],
-      dataZoom: [
-        {
-          type: 'inside',
-          xAxisIndex: [0, 1],
-          start: 98,
-          end: 100
-        },
-        {
-          show: true,
-          xAxisIndex: [0, 1],
-          type: 'slider',
-          top: '85%',
-          start: 98,
-          end: 100
-        }
-      ],
-      series: [
-        {
-          name: 'Dow-Jones index',
-          type: 'candlestick',
-          data: data.values,
-          itemStyle: {
-            color: upColor,
-            color0: downColor,
-            borderColor: undefined,
-            borderColor0: undefined
-          }
-        },
-        {
-          name: 'MA5',
-          type: 'line',
-          data: calculateMA(5, data),
-          smooth: true,
-          lineStyle: {
-            opacity: 0.5
-          }
-        },
-        {
-          name: 'MA10',
-          type: 'line',
-          data: calculateMA(10, data),
-          smooth: true,
-          lineStyle: {
-            opacity: 0.5
-          }
-        },
-        {
-          name: 'MA20',
-          type: 'line',
-          data: calculateMA(20, data),
-          smooth: true,
-          lineStyle: {
-            opacity: 0.5
-          }
-        },
-        {
-          name: 'MA30',
-          type: 'line',
-          data: calculateMA(30, data),
-          smooth: true,
-          lineStyle: {
-            opacity: 0.5
-          }
-        },
-        {
-          name: 'Volume',
-          type: 'bar',
-          xAxisIndex: 1,
-          yAxisIndex: 1,
-          data: data.volumes
+          value: -1,
+          color: upColor
         }
       ]
-    }),
-    true
-  );
+    },
+    grid: [
+      {
+        left: '10%',
+        right: '8%',
+        height: '50%'
+      },
+      {
+        left: '10%',
+        right: '8%',
+        top: '63%',
+        height: '16%'
+      }
+    ],
+    xAxis: [
+      {
+        type: 'category',
+        data: data.categoryData,
+        boundaryGap: false,
+        axisLine: { onZero: false },
+        splitLine: { show: false },
+        min: 'dataMin',
+        max: 'dataMax',
+        axisPointer: {
+          z: 100
+        }
+      },
+      {
+        type: 'category',
+        gridIndex: 1,
+        data: data.categoryData,
+        boundaryGap: false,
+        axisLine: { onZero: false },
+        axisTick: { show: false },
+        splitLine: { show: false },
+        axisLabel: { show: false },
+        min: 'dataMin',
+        max: 'dataMax'
+      }
+    ],
+    yAxis: [
+      {
+        scale: true,
+        splitArea: {
+          show: true
+        }
+      },
+      {
+        scale: true,
+        gridIndex: 1,
+        splitNumber: 2,
+        axisLabel: { show: false },
+        axisLine: { show: false },
+        axisTick: { show: false },
+        splitLine: { show: false }
+      }
+    ],
+    dataZoom: [
+      {
+        type: 'inside',
+        xAxisIndex: [0, 1],
+        start: 98,
+        end: 100
+      },
+      {
+        show: true,
+        xAxisIndex: [0, 1],
+        type: 'slider',
+        top: '85%',
+        start: 98,
+        end: 100
+      }
+    ],
+    series: [
+      {
+        name: 'Dow-Jones index',
+        type: 'candlestick',
+        data: data.values,
+        itemStyle: {
+          color: upColor,
+          color0: downColor,
+          borderColor: undefined,
+          borderColor0: undefined
+        }
+      },
+      {
+        name: 'MA5',
+        type: 'line',
+        data: calculateMA(5, data),
+        smooth: true,
+        lineStyle: {
+          opacity: 0.5
+        }
+      },
+      {
+        name: 'MA10',
+        type: 'line',
+        data: calculateMA(10, data),
+        smooth: true,
+        lineStyle: {
+          opacity: 0.5
+        }
+      },
+      {
+        name: 'MA20',
+        type: 'line',
+        data: calculateMA(20, data),
+        smooth: true,
+        lineStyle: {
+          opacity: 0.5
+        }
+      },
+      {
+        name: 'MA30',
+        type: 'line',
+        data: calculateMA(30, data),
+        smooth: true,
+        lineStyle: {
+          opacity: 0.5
+        }
+      },
+      {
+        name: 'Volume',
+        type: 'bar',
+        xAxisIndex: 1,
+        yAxisIndex: 1,
+        data: data.volumes
+      }
+    ]
+  };
+  myChart.setOption(chartOption);
 
   // const len = data.categoryData.length;
   // myChart.dispatchAction({
