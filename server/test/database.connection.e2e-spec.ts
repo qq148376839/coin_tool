d35261f5fb20b17611449070d9c1900060b9415f
riowang(王腾) <riowang@futunn.com>
2025-04-07 10:49:09 +0800

    初步测试

--- server/test/database.connection.e2e-spec.ts
+++ server/test/database.connection.e2e-spec.ts
@@ -49,10 +49,11 @@ describe('Database Connection Test', () => {
 
   it('should create tables', async () => {
     try {
-      // 使用 OptionContract 模型来查询表
       const tables = await OptionContract.sequelize.showAllSchemas({ logging: console.log });
       console.log('Created tables:', tables);
-      expect(tables).toEqual(
+      // 提取表名
+      const tableNames = tables.map(table => table.Tables_in_longport);
+      expect(tableNames).toEqual(
         expect.arrayContaining(['OptionContracts', 'OptionOrders', 'OptionPositions'])
       );
     } catch (error) {
