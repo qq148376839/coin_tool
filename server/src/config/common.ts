8d57b7a34ffa24121a256a96fd8cead7fe434c50
riowang(王腾) <riowang@futunn.com>
2025-04-07 09:55:25 +0800

    初步测试

--- server/src/config/common.ts
+++ server/src/config/common.ts
@@ -1,28 +1,29 @@
 import { SequelizeModuleOptions } from '@nestjs/sequelize';
-function configOptions() {
+
+const configOptions = () => {
   const config: any = {};
 
   config.longPort = {
-    appKey: process.env.LONGPORT_APP_KEY || 'xxx',
-    appSecret: process.env.LONGPORT_APP_SECRET || 'xxx',
-    accessToken: process.env.LONGPORT_ACCESS_TOKEN || 'xxx',
+    appKey: process.env.LONGPORT_APP_KEY || '',
+    appSecret: process.env.LONGPORT_APP_SECRET || '',
+    accessToken: process.env.LONGPORT_ACCESS_TOKEN || '',
     region: process.env.LONGPORT_REGION || 'cn',
     enableOvernight: process.env.LONGPORT_ENABLE_OVERNIGHT === 'true'
   };
 
   const sequelizeConfig: SequelizeModuleOptions = {
     dialect: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: 'xx',
-    database: 'xx',
+    host: process.env.DB_HOST || 'localhost',
+    port: Number(process.env.DB_PORT) || 3306,
+    username: process.env.DB_USERNAME || 'root',
+    password: process.env.DB_PASSWORD || 'qwer1234!',
+    database: process.env.DB_DATABASE || 'longport',
     autoLoadModels: true,
     synchronize: true,
   };
   config.sequelizeConfig = sequelizeConfig;
 
   return config;
-}
+};
 
-export default configOptions();
+export default configOptions;
