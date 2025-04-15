18480c49657644c67b4793495020fa2670f83967
riowang(王腾) <riowang@futunn.com>
2025-04-15 14:41:48 +0800

    更新为长桥api

--- server/src/modules/user/userController.ts
+++ server/src/modules/user/userController.ts
@@ -1,27 +1,17 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { UserService } from './userService';
-import { PriceDto } from '../../dto/market.dtc';
+
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Get('getUserInfo')
-  // 设置请求头
-  async getPrice(): Promise<PriceDto> {
-    try {
-      return this.userService.getUserInfo();
-    } catch (error) {
-      console.log(error);
-      throw new Error('获取用户信息失败');
-    }
+  @Get('info')
+  async getUserInfo() {
+    return await this.userService.getUserInfo();
   }
-  @Get('getUserContractNews')
-  async getUserContractNews(@Query() params: any): Promise<PriceDto> {
-    try {
-      return this.userService.getUserContractNews(params);
-    } catch (error) {
-      console.log(error);
-      throw new Error('获取用户信息失败');
-    }
+
+  @Get('contract-news')
+  async getUserContractNews() {
+    return await this.userService.getUserContractNews();
   }
 }
--- server/src/modules/user/userService.ts
+++ server/src/modules/user/userService.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { LongPortAccountService } from '../../services/longport.account.service';
+import { AccountBalance, CashInfo, MarginRatio } from '../../types/longport.types';
 
 @Injectable()
 export class UserService {
   constructor(private readonly longPortAccountService: LongPortAccountService) {}
 
   // 获取当前账号信息
-  async getUserInfo(): Promise<any> {
+  async getUserInfo(): Promise<{
+    balance: AccountBalance;
+    cashInfo: CashInfo;
+  }> {
     try {
       const [balance, cashInfo] = await Promise.all([
         this.longPortAccountService.getAccountBalance(),
@@ -18,18 +22,17 @@ export class UserService {
         cashInfo
       };
     } catch (error) {
-      console.log(error);
+      console.error('获取用户信息失败:', error);
       throw new Error('获取用户信息失败');
     }
   }
 
   // 获取用户合约信息
-  async getUserContractNews(): Promise<any> {
+  async getUserContractNews(): Promise<MarginRatio> {
     try {
-      const marginRatio = await this.longPortAccountService.getMarginRatio();
-      return marginRatio;
+      return await this.longPortAccountService.getMarginRatio();
     } catch (error) {
-      console.log(error);
+      console.error('获取用户合约信息失败:', error);
       throw new Error('获取用户合约信息失败');
     }
   }
