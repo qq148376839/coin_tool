3bc67980bb765f6aec218f56f89d767040ad8a02
riowang(王腾) <riowang@futunn.com>
2025-04-15 15:23:30 +0800

    更新为长桥api

--- client/src/main.ts
+++ client/src/main.ts
@@ -5,7 +5,6 @@ import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import App from './App.vue'
 import router from './router'
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 
 const app = createApp(App);
 
@@ -14,17 +13,5 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
 }
 app.use(router)
 app.use(ElementPlus)
-
-const config = new DocumentBuilder()
-  .setTitle('长桥API接口文档')
-  .setDescription('加密货币交易工具API文档')
-  .setVersion('1.0')
-  .addTag('account', '账户相关接口')
-  .addTag('quote', '行情相关接口')
-  .addTag('order', '订单相关接口')
-  .build()
-
-const document = SwaggerModule.createDocument(app, config)
-SwaggerModule.setup('api', app, document)
 
 app.mount('#app')
